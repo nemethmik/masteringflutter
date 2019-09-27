25bedb78bd8de125248ae698686bdeede21f142f
Miklos Nemeth <nemethmiklos1960@gmail.com>
2019-09-27 13:07:08 +0200

    Disposable Stream Builder on Home Page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,41 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:masteringflutter/category.dart';
 import 'package:masteringflutter/categorypage.dart';
-import 'package:provider/provider.dart';
+import 'disposable.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider<CategoriesBloc>(
-      builder: (context) => CategoriesBloc(),
-      dispose: (context,bloc) => bloc?.dispose(),
-      child: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Text("E-Commerce"),
         ),
-        body: Consumer<CategoriesBloc>(
-          builder: (context,bloc,child) => StreamBuilder<List<Category>>(
-            stream: bloc.categories,
-            builder: (context,snapshot){
-              if(snapshot.hasData) {
-                return ListView.builder(
-                  itemBuilder: (context,index){
-                    try {
-                      return 
-                      ListTile(
-                        onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => CategoryPage(snapshot.data[index]),
-                          )),
-                        title: Text(snapshot.data[index].name,
-                            style: TextStyle(fontSize: 24.0),
-                          ),
-                      );
-                    } catch(_){ return null;}
-                },);
-              } else return Center(child: CircularProgressIndicator(),);
-            },),
-        )
-      ),
+        body: DisposableStream<CategoriesBloc,List<Category>>(
+          value: CategoriesBloc(),
+          stream: (bloc) => bloc.categories,
+          builder: (context,snapshot){
+            if(snapshot.hasData) {
+              return ListView.builder(
+                itemBuilder: (context,index){
+                  try {
+                    return 
+                    ListTile(
+                      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => CategoryPage(snapshot.data[index]),
+                        )),
+                      title: Text(snapshot.data[index].name,
+                          style: TextStyle(fontSize: 24.0),
+                        ),
+                    );
+                  } catch(_){ return null;}
+              },);
+            } else return Center(child: CircularProgressIndicator(),);
+          },),
     );
   }
 }
